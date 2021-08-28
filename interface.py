f1aa2de10026169d6bb81af148bde71415d2c34e
dijaidy <65172181+dijaidy@users.noreply.github.com>
2021-08-28 16:14:48 +0900

    commit

--- interface.py
+++ interface.py
@@ -5,9 +5,9 @@ window = Tk()
 window.title("MOBILE_STUDY")
 window.geometry("400x800")
 
-font = ("BMJUA_ttf",)
-label = Label(window, text="MOBILE\nSTUDY")
-label.place(relx=0, relwidth=1, rely=0, height=50)
+title_font = ("배달의민족 주아", 30)
+label = Label(window, text="MOBILE\nSTUDY", font=title_font)
+label.place(relx=0, relwidth=1, rely=0, height=150)
 
 
 window.resizable(width=False, height=False)
