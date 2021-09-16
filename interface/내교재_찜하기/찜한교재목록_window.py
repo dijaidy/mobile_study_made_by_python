9f4cb92b73f1df2b8b524cc61ddeef566a182e5d
IWNT <smartapple03@gmail.com>
2021-09-16 21:54:13 +0900

    9/16커밋
    listbox 이론상으로 구현완료
    버그만 고치면 됨

--- interface/내교재_찜하기/찜한교재목록_window.py
+++ interface/내교재_찜하기/찜한교재목록_window.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from tkinter import ttk as tk
+
 import sys
 import os
 import json
+from PIL import Image, ImageTk
+import webbrowser
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 sys.path.append(
@@ -9,14 +13,138 @@ sys.path.append(
 )
 from information_management.user_information import 찜한교재_manage_user_information
 
+# 폰트 설정
+title_font = ("배달의민족 주아", 23)
+menu_font = ("배달의민족 주아", 15)
+submenu_font = ("배달의민족 주아", 17)
 
-class 찜한교재목록_window:
+class 찜한교재목록_window(찜한교재_manage_user_information):
     def __init__(self):
+
+        #찜한교재_manage_user_information 상속받기
+        super().__init__()
+
         # 창 설정
         self.window = Tk()
         self.window.title("찜한교재현황")
         self.window.geometry("400x800")
-        self.window.resizable(False, False)
-        frame = Frame(self.window)
-        scrollbar = Scrollbar(frame)
-        scrollbar.pack(side="right", fill="y")
+
+        #이전 교재 보여주기 버튼
+        self.prior_button=Button(self.window, text="이전 교재로 가기", font=menu_font, command=lambda: self.show_book_information(-1))
+        self.prior_button.place(relx=0, y=750, height=50, width=200)
+
+        #다음 교재 보여주기 버튼
+        self.next_button=Button(self.window, text="다음 교재로 가기", font=menu_font, command=lambda: self.show_book_information(1))
+        self.next_button.place(relx=1/2, y=750, height=50, width=200)
+
+        #분류기준체크박스만들기
+        self.combobox=Listbox(self.window, selectmode="extended", height=0, width=150)
+        self.combobox.insert(0, "과목별 분류")
+        self.combobox.insert(1, "성취도순 분류")
+        self.combobox.place(x=0, y=0)
+
+        #
+        self.combobox1=Listbox(self.window, selectmode="extended", height=0, width=150)
+        self.combobox1.pack()
+        
+        #분류기준확인버튼만들기
+        self.check_standard=Button(self.window, text="확인 후 분류기준 선택하기", font=("배달의민족 주아", 10), command=lambda: self.init_checkbox(self.combobox.get(self.combobox.curselection()[0], self.combobox.curselection()[0])))
+        self.check_standard.place(x=150, y=0)
+
+        #최종확인버튼만들기
+        self.showing_button=Button(self.window, text="확인 후 교재보기", font=("배달의민족 주아", 10), command=lambda: self.show_book_information(first_show_standard=self.combobox.get(self.combobox.curselection()[0], self.combobox.curselection()[0]), second_show_standard=self.combobox1.get(self.combobox1.curselection()[0], self.combobox1.curselction()[0])))
+        self.showing_button.place(x=300, y=0)
+
+        # 교재 이미지
+        self.book_image = Label(self.window, borderwidth=2, relief="sunken", text="자료 없음")
+        self.book_image.place(x=25, y=75, height=250, width=200)
+
+        # 교재 타이틀
+        self.book_title = Message(self.window, font=title_font)
+        self.book_title.place(x=0, y=350, height=200, width=400)
+
+        # 교재 정보
+        self.book_info = Label(self.window, font=menu_font, justify=LEFT, padx=1, pady=1)
+        self.book_info.place(x=25, y=550, height=150, width=400 - 50)
+
+        self.open_web_button = Button(
+            self.window, font=menu_font, text="교재\n웹사이트\n오픈", padx=1, pady=1
+        )
+        self.open_web_button.place(x=250, y=75, width=125, height=125)
+
+        #기타 인스턴스변수 생성
+        self.image_index=0
+        self.present_book={}
+        self.present_book_title=""
+
+    #분류기준에 따른 체크박스 생성
+    def init_checkbox(self, standard=""):
+        if (standard=="과목별 분류"):
+            self.values=["국어", "수학", "영어", "사회", "역사", "과학", "기술.가정"]
+            for i in range(0, 7):
+                self.combobox1.insert(i, self.values[i])
+            self.combobox1.place(self.window, width=0, height=0, x=0, y=20)
+        elif (standard=="성취도순 분류"):
+            self.values=["0%~20%", "21%~40%", "41%~60%", "61%~80%", "81%~100%"]
+            for i in range(0, 7):
+                self.combobox1.insert(i, self.values[i])
+            self.combobox1.place(self.window, width=0, height=0, x=0, y=20)
+            
+
+    def show_book_information(self, index_moving=0, first_show_standard="", second_show_standard=""):  # 알라딘api에서 가져온 책 정보를 이용해 띄워줌
+        # 찜한 책 모음 가져오기
+        searching_result={}
+        chosen_book_keys=self.chosen_book.keys()
+        if first_show_standard=="과목별 분류":
+            for i in chosen_book_keys:
+                if self.chosen_book[i]["subject"]==second_show_standard:
+                    searching_result.append(self.chosen_book[i])
+        elif first_show_standard=="성취도순 분류":
+            for i in chosen_book_keys:
+                if int(second_show_standard.split('~')[0].replace("%", ""))<=self.chosen_book[i]["acheivement"]<=int(second_show_standard.split('~')[1].replace("%", "")):
+                    searching_result.append(self.chosen_book[i])
+
+        # 인덱스 조정
+        self.image_index += index_moving
+        if self.image_index < -1 * len(searching_result):
+            self.image_index + len(searching_result)
+        elif self.image_index >= len(searching_result):
+            self.image_index - len(searching_result)
+
+        # 책제목 리스트
+        title_list = list(searching_result.keys())
+
+        # curl 요청
+        # curl "이미지 주소" > "저장 될 이미지 파일 이름"
+        book = searching_result[title_list[self.image_index]]
+        self.present_book = book
+        self.present_book_title = title_list[self.image_index]
+        url = book["cover"]
+
+        os.system("curl " + url + " > image_sources\교재선택_image_file.jpg")
+
+        # 이미지 크기 조정
+        size_adjusting_image = Image.open("image_sources\교재선택_image_file.jpg")
+        image = size_adjusting_image.resize(
+            (size_adjusting_image.size[0] * 2, size_adjusting_image.size[1] * 2), Image.ANTIALIAS
+        )
+
+        resized_image = ImageTk.PhotoImage(image, master=self.window)  # 새창에서 그림띄우면 마스터 정의 꼭!
+
+        self.book_image.config(image=resized_image, text="")
+
+        # 타이틀 수정
+        book_title_text = title_list[self.image_index]
+        self.book_title.config(text=book_title_text)
+
+        # 정보 수정
+        book_info_text = "가격: %s원" % book["priceStandard"]
+        self.book_info.config(text=book_info_text)
+
+        # 링크 수정
+        def open_web():
+            webbrowser.open(book["link"])
+
+        self.open_web_button.config(command=open_web)
+
+        self.window.mainloop()
