3f0fb816ec853ba64e102193e93f5df99615a48a
IWNT <smartapple03@gmail.com>
2021-09-17 00:30:56 +0900

    찜한교재목록 완성?
    이론상 완벽.

--- interface/내교재_찜하기/찜한교재목록_window.py
+++ interface/내교재_찜하기/찜한교재목록_window.py
@@ -29,87 +29,97 @@ class 찜한교재목록_window(찜한교재_manage_user_information):
         self.window.title("찜한교재현황")
         self.window.geometry("400x800")
 
+        #책 전체개수/현재 위치
+        self.searching_order = Label(self.window, font=menu_font, text="  /  ")
+        self.searching_order.place(x=250, y=30, width=125, height=50)
+
         #이전 교재 보여주기 버튼
-        self.prior_button=Button(self.window, text="이전 교재로 가기", font=menu_font, command=lambda: self.show_book_information(-1))
-        self.prior_button.place(relx=0, y=750, height=50, width=200)
+        self.prior_button = Button(self.window, text = "이전 교재로 가기", font = menu_font, command = lambda: self.show_book_information(index_moving = -1, second_show_standard = self.combobox1.get(self.combobox1.curselection()[0], self.combobox1.curselection()[0])[0]))
+        self.prior_button.place(relx = 0, y = 750, height = 50, width = 200)
 
         #다음 교재 보여주기 버튼
-        self.next_button=Button(self.window, text="다음 교재로 가기", font=menu_font, command=lambda: self.show_book_information(1))
-        self.next_button.place(relx=1/2, y=750, height=50, width=200)
+        self.next_button = Button(self.window, text = "다음 교재로 가기", font = menu_font, command = lambda: self.show_book_information(index_moving = 1, second_show_standard = self.combobox1.get(self.combobox1.curselection()[0], self.combobox1.curselection()[0])[0]))
+        self.next_button.place(relx = 1/2, y = 750, height = 50, width = 200)
 
-        #분류기준체크박스만들기
-        self.combobox=Listbox(self.window, selectmode="extended", height=0, width=150)
+        #큰 분류기준체크박스만들기
+        self.combobox = Listbox(self.window, selectmode = "extended", height = 0, width = 0)
         self.combobox.insert(0, "과목별 분류")
         self.combobox.insert(1, "성취도순 분류")
-        self.combobox.place(x=0, y=0)
+        self.combobox.place(x = 0, y = 0)
 
-        #
-        self.combobox1=Listbox(self.window, selectmode="extended", height=0, width=150)
-        self.combobox1.pack()
+        #작은 분류체크박스만들기
+        self.combobox1 = Listbox(self.window, selectmode = "extended", height = 0, width = 0)
+        self.combobox1.place(x = 80, y = 0)
         
-        #분류기준확인버튼만들기
-        self.check_standard=Button(self.window, text="확인 후 분류기준 선택하기", font=("배달의민족 주아", 10), command=lambda: self.init_checkbox(self.combobox.get(self.combobox.curselection()[0], self.combobox.curselection()[0])))
-        self.check_standard.place(x=150, y=0)
+        #분류기준확인버튼만들기 "확인 후 분류기준 선택하기"
+        self.check_standard = Button(self.window, text = "큰 분류기준 선택하기", font = ("배달의민족 주아", 10), command = lambda: self.init_checkbox(self.combobox.get(self.combobox.curselection()[0], self.combobox.curselection()[0])[0]))
+        self.check_standard.place(x = 175, y = 5)
 
         #최종확인버튼만들기
-        self.showing_button=Button(self.window, text="확인 후 교재보기", font=("배달의민족 주아", 10), command=lambda: self.show_book_information(first_show_standard=self.combobox.get(self.combobox.curselection()[0], self.combobox.curselection()[0]), second_show_standard=self.combobox1.get(self.combobox1.curselection()[0], self.combobox1.curselction()[0])))
-        self.showing_button.place(x=300, y=0)
+        self.showing_button = Button(self.window, text = "작은 분류기준 선택하고 교재보기", font = ("배달의민족 주아", 10), command = lambda: self.show_book_information(second_show_standard = self.combobox1.get(self.combobox1.curselection()[0], self.combobox1.curselection()[0])[0]))
+        self.showing_button.place(x = 300, y = 5)
 
         # 교재 이미지
-        self.book_image = Label(self.window, borderwidth=2, relief="sunken", text="자료 없음")
-        self.book_image.place(x=25, y=75, height=250, width=200)
+        self.book_image = Label(self.window, borderwidth = 2, relief = "sunken", text = "자료 없음")
+        self.book_image.place(x = 25, y = 120, height = 250, width = 200)
 
         # 교재 타이틀
-        self.book_title = Message(self.window, font=title_font)
-        self.book_title.place(x=0, y=350, height=200, width=400)
+        self.book_title = Message(self.window, font = title_font)
+        self.book_title.place(x = 0, y = 350, height = 200, width = 400)
 
         # 교재 정보
-        self.book_info = Label(self.window, font=menu_font, justify=LEFT, padx=1, pady=1)
-        self.book_info.place(x=25, y=550, height=150, width=400 - 50)
+        self.book_info = Label(self.window, font = menu_font, justify = LEFT, padx = 1, pady = 1)
+        self.book_info.place(x = 25, y = 550, height = 150, width = 400 - 50)
 
         self.open_web_button = Button(
-            self.window, font=menu_font, text="교재\n웹사이트\n오픈", padx=1, pady=1
+            self.window, font = menu_font, text = "교재\n웹사이트\n오픈", padx = 1, pady = 1
         )
-        self.open_web_button.place(x=250, y=75, width=125, height=125)
+        self.open_web_button.place(x = 250, y = 75, width = 125, height = 125)
+
+        #성취도
+        self.book_acheivement = DoubleVar()
+        self.acheivement_bar = tk.Progressbar(self.window, maximum = 100, length = 175, variable = self.book_acheivement)
+        self.acheivement_bar.place(x = 225, y = 230)
 
         #기타 인스턴스변수 생성
-        self.image_index=0
-        self.present_book={}
-        self.present_book_title=""
+        self.image_index = 0
+        self.present_book = {}
+        self.present_book_title = ""
 
     #분류기준에 따른 체크박스 생성
-    def init_checkbox(self, standard=""):
-        if (standard=="과목별 분류"):
-            self.values=["국어", "수학", "영어", "사회", "역사", "과학", "기술.가정"]
+    def init_checkbox(self, standard = ""):
+        self.combobox1.delete(0, self.combobox1.size())
+        if (standard == "과목별 분류"):
+            self.values = ["국어", "수학", "영어", "사회", "역사", "과학", "기술.가정"]
             for i in range(0, 7):
                 self.combobox1.insert(i, self.values[i])
-            self.combobox1.place(self.window, width=0, height=0, x=0, y=20)
-        elif (standard=="성취도순 분류"):
-            self.values=["0%~20%", "21%~40%", "41%~60%", "61%~80%", "81%~100%"]
-            for i in range(0, 7):
+            self.combobox1.place(width = 80, height = 120, x = 80, y = 0)
+        elif (standard == "성취도순 분류"):
+            self.values = ["0%~20%", "21%~40%", "41%~60%", "61%~80%", "81%~100%"]
+            for i in range(0, 5):
                 self.combobox1.insert(i, self.values[i])
-            self.combobox1.place(self.window, width=0, height=0, x=0, y=20)
+            self.combobox1.place(width = 80, height = 120, x = 80, y = 0)
             
 
-    def show_book_information(self, index_moving=0, first_show_standard="", second_show_standard=""):  # 알라딘api에서 가져온 책 정보를 이용해 띄워줌
+    def show_book_information(self, index_moving = 0, second_show_standard = ""):  # 알라딘api에서 가져온 책 정보를 이용해 띄워줌
         # 찜한 책 모음 가져오기
-        searching_result={}
-        chosen_book_keys=self.chosen_book.keys()
-        if first_show_standard=="과목별 분류":
+        searching_result = {}
+        chosen_book_keys = self.chosen_book.keys()
+        if second_show_standard in ["국어", "수학", "영어", "사회", "역사", "과학", "기술.가정"]:
             for i in chosen_book_keys:
-                if self.chosen_book[i]["subject"]==second_show_standard:
+                if self.chosen_book[i]["subject"] == second_show_standard:
                     searching_result.append(self.chosen_book[i])
-        elif first_show_standard=="성취도순 분류":
+        elif second_show_standard in ["0%~20%", "21%~40%", "41%~60%", "61%~80%", "81%~100%"]:
             for i in chosen_book_keys:
-                if int(second_show_standard.split('~')[0].replace("%", ""))<=self.chosen_book[i]["acheivement"]<=int(second_show_standard.split('~')[1].replace("%", "")):
+                if int(second_show_standard.split('~')[0].replace("%", "")) <= self.chosen_book[i]["acheivement"] <= int(second_show_standard.split('~')[1].replace("%", "")):
                     searching_result.append(self.chosen_book[i])
 
         # 인덱스 조정
         self.image_index += index_moving
-        if self.image_index < -1 * len(searching_result):
-            self.image_index + len(searching_result)
-        elif self.image_index >= len(searching_result):
-            self.image_index - len(searching_result)
+        if self.image_index < len(searching_result):
+            self.image_index += len(searching_result)
+        elif self.image_index == len(searching_result):
+            self.image_index = 0
 
         # 책제목 리스트
         title_list = list(searching_result.keys())
@@ -129,22 +139,26 @@ class 찜한교재목록_window(찜한교재_manage_user_information):
             (size_adjusting_image.size[0] * 2, size_adjusting_image.size[1] * 2), Image.ANTIALIAS
         )
 
-        resized_image = ImageTk.PhotoImage(image, master=self.window)  # 새창에서 그림띄우면 마스터 정의 꼭!
+        resized_image = ImageTk.PhotoImage(image, master = self.window)  # 새창에서 그림띄우면 마스터 정의 꼭!
 
-        self.book_image.config(image=resized_image, text="")
+        self.book_image.config(image = resized_image, text = "")
 
         # 타이틀 수정
         book_title_text = title_list[self.image_index]
-        self.book_title.config(text=book_title_text)
+        self.book_title.config(text = book_title_text)
 
         # 정보 수정
         book_info_text = "가격: %s원" % book["priceStandard"]
-        self.book_info.config(text=book_info_text)
+        self.book_info.config(text = book_info_text)
+
+        #인덱스 수정
+        searching_order_text = "%s / %s" % (len(searching_result), self.image_index)
+        self.searching_order.config(text=searching_order_text)
 
         # 링크 수정
         def open_web():
             webbrowser.open(book["link"])
 
-        self.open_web_button.config(command=open_web)
+        self.open_web_button.config(command = open_web)
 
         self.window.mainloop()
